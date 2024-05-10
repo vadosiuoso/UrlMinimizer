c77e1b2e4936f9235b6886107253351a1cb7c509
vadosiuoso <trezstyle@gmail.com>
2024-05-10 23:25:06 +0200

    refactor

--- src/main/java/com/example/demo/anotation/UniqueUsername.java
+++ src/main/java/com/example/demo/anotation/UniqueUsername.java
@@ -1,12 +1,19 @@
 package com.example.demo.anotation;
 
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 import jakarta.validation.Payload;
 import jakarta.validation.Constraint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.lang.annotation.*;
 
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = UniqueUsernameValidator.class)
+@Constraint(validatedBy = UniqueUsername.UniqueUsernameValidator.class)
 @Documented
 public @interface UniqueUsername {
   String message() default "This username is already taken";
@@ -14,4 +21,17 @@ public @interface UniqueUsername {
   Class<?>[] groups() default {};
 
   Class<? extends Payload>[] payload() default {};
+
+
+  @RequiredArgsConstructor
+  class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+
+    @Autowired
+    private final UserService userService;
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
+        User user = userService.findByUsername(username);
+        return user == null;
+    }
+  }
 }
